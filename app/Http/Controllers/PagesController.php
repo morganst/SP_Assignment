3e30fa0ce1e71cac40e67960bab08d3b9885062f
Morgan Sturtz <msturtz@my.apsu.edu>
2018-08-04 18:29:19 -0400

    updated and added some pages

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index() {
+        $title = "Volunteer Page";
+        //return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about() {
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
+    }
+
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Welcome to the Volunteer Page</p>
+@endsection
