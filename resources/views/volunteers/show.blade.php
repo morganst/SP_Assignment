6020fb519b7f8e2662cb96fc4f51ee029234b02d
morgansturtz <msturtz@my.apsu.edu>
2018-08-08 15:55:50 -0400

    styling

--- resources/views/volunteers/show.blade.php
+++ resources/views/volunteers/show.blade.php
@@ -3,8 +3,20 @@
 @section('content')
     <h2>{{$vols->firstName}} {{$vols->lastName}}</h2>
     <div class="">Here is where you put information about the volunteer pulled from db</div>
-    <div class="text-right">
-        <a href="/volunteers" class="btn btn-sm btn-primary" role="button" aria-pressed="true">Back</a>
-    </div>
-    <hr><small>Created: {{$vols->created_at}}</small></hr>
+    <hr>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+        <div>username: {{$vols->userName}}</div>
+    </hr>
+    <hr>
+        <small>Created: {{$vols->created_at}}</small>
+        <div class="text-right">
+            <a href="/volunteers" class="btn btn-primary" role="button" aria-pressed="true">Back</a>
+        </div>
+    </hr>
 @endsection
