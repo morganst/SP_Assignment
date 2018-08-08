43170abf18237c8ef1a15f46c77edc219afebda1
Morgan Sturtz <msturtz@my.apsu.edu>
2018-08-07 21:50:01 -0400

    Form for creating new volunteer

--- resources/views/volunteers/create.blade.php
+++ resources/views/volunteers/create.blade.php
@@ -2,5 +2,26 @@
 
 @section('content')
     <h1>New Volunteer</h1>
-    <p>Here you can view and edit volunteers</p>
+    <p>Please enter information for creating a new volunteer</p>
+    {!! Form::open(['action' => 'VolunteersController@store', 'method' => 'POST']) !!}
+        <form>
+            <div class="form-row">
+                <div class="col col-md-2">
+                        {{Form::text('firstName', '', ['class' => 'form-control', 'placeholder' => 'First Name'])}}
+                </div>
+                <div class="col col-md-2">
+                        {{Form::text('lastName', '', ['class' => 'form-control', 'placeholder' => 'Last Name'])}}
+                </div>
+            </div>
+            &nbsp;
+            <div class="form-row">
+                <div class="col col-md-2">
+                    {{Form::text('userName', '', ['class' => 'form-control', 'placeholder' => 'User Name'])}}
+                </div>
+                <div class="col col-md-2">
+                    {{Form::text('password', '', ['class' => 'form-control', 'placeholder' => 'Password'])}}
+                </div>
+            </div>
+        </form>
+    {!! Form::close() !!}
 @endsection
