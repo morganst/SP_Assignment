94270a6bc7c47d061df0196b400be1b512fc2971
morgansturtz <msturtz@my.apsu.edu>
2018-08-15 14:54:23 -0400

    cleanup

--- resources/views/volunteers/index.blade.php
+++ resources/views/volunteers/index.blade.php
@@ -1,58 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <style>
-    li a, .dropbtn {
-        display: inline-block;
-        color: white;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-    }
-    li.dropdown {
-        display: inline-block;
-    }
-    .dropdown-content {
-        display: none;
-        position: absolute;
-        background-color: #f9f9f9;
-        min-width: 160px;
-        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-        z-index: 5000;
-    }
-    .dropdown-content a {
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        display: block;
-        text-align: left;
-    }
-    .dropdown-content a:hover {background-color: #f1f1f1}
-    .dropdown:hover .dropdown-content {
-        display: block;
-    }
-    </style>
     <h1>Volunteer Index</h1>
     <div>Here you can view and edit volunteers</div>
-    <div class="text-right"><a href="/volunteers/create" class="btn btn-md btn-success">Add New</a></div>  
+    <div class="text-right"><a href="/volunteers/create" class="btn btn-md btn-primary">Add New</a></div>  
     <form class="form-inline my-2 my-md-0 nav" role="search" method="get" action="{{url("/search")}}">
             <div class="input-group">
                 <input type="text" class="form-control mr-sm-2" placeholder="Search" name="title">
-                <div class="input-group-btn">
-                    <button class="btn btn-success my-2 my-sm-0" type="submit"><i class="glyphicon glyphicon-search"></i>Search</button>
-                </div>
+                <button class="btn btn-success my-2 my-sm-0" type="submit"><i class="glyphicon glyphicon-search"></i>Search</button>
             </div>
     </form> 
     <br>
     <li class="dropdown">
-        <a href="javascript:void(0)" class="btn btn-md btn-success">Volunteer Filter</a>
+        <a href="javascript:void(0)" class="btn btn-md btn-success">Volunteer Filter <span class="fas fa-caret-down"/></a>
         <div class="dropdown-content">
-          <a href="/multiFilter">Approved/Pending</a>
-          <a href="/filter?title=Approved">Approved</a>
-          <a href="/filter?title=Pending Approval">Pending Approval</a>
-          <a href="/filter?title=Disapproved">Disapproved</a>
-          <a href="/filter?title=Inactive">Inactive</a>
-          <a href="/volunteers">All</a>
+            <a href="/multiFilter">Approved/Pending</a>
+            <a href="/filter?title=Approved">Approved</a>
+            <a href="/filter?title=Pending Approval">Pending Approval</a>
+            <a href="/filter?title=Disapproved">Disapproved</a>
+            <a href="/filter?title=Inactive">Inactive</a>
+            <a href="/volunteers">All</a>
         </div>
     </li>
     <hr>
@@ -95,4 +62,37 @@
         <p>No volunteers found</p>
     @endif
     </hr>
+
+    <style>
+        li a, .dropbtn {
+            display: inline-block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+        li.dropdown {
+            display: inline-block;
+        }
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            background-color: #f9f9f9;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 5000;
+        }
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+            text-align: left;
+        }
+        .dropdown-content a:hover {background-color: #f1f1f1}
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+    </style>
+
 @endsection
